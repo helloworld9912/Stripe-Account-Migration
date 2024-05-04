dce3cc02f244aa630747232a6e3743576b950767
bapaero <49796533+helloworld9912@users.noreply.github.com>
2024-05-04 12:31:31 +0200

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -15,6 +15,13 @@ interface MigrationConfig {
       PRICES: true,
     },
   };
+
+  const INVOICES_CONFIG = {
+    ONLY_PAID_INVOICES: true, //paid invoice contain refunded invoices too
+    EXCLUDE_0_AMOUNT_INVOICES: true, //exclude invoices with 0 amount (free trials, etc.)
+    EXPORT_JSON: true, //export invoices to a json file (to be used later for importing into your database)
+  };
+
   
-  export { CONFIG };
+  export { CONFIG, INVOICES_CONFIG };
   
