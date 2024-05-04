e5602259c3091147fc40a66f84683d07bbf919a8
bapaero <49796533+helloworld9912@users.noreply.github.com>
2024-05-04 12:31:37 +0200

    Update inventory.ts

--- src/inventory.ts
+++ src/inventory.ts
@@ -6,29 +6,32 @@ import { getAllProducts } from "./scripts/products";
 import { getAllPromotionCodes } from "./scripts/promotion_codes";
 import { getAllScriptionSchedules } from "./scripts/subscription_schedules";
 import { getAllSubscriptions } from "./scripts/subscriptions";
+import { getAllInvoices } from "./scripts/invoices";
 
 async function startMigration(): Promise<void> {
   try {
     console.log(
       "Starting the inventory process of your Stripe data [SOURCE ACCOUNT]..."
     );
-    const coupons = await getAllCoupons();
-    console.log(`Total coupons to migrate: ${coupons.length}`);
+  
     const paymentLinks = await getAllPaymentLinks();
     console.log(`Total payment links to migrate: ${paymentLinks.length}`);
     const prices = await getAllPrices();
     console.log(`Total prices to migrate: ${prices.length}`);
     const products = await getAllProducts();
     console.log(`Total products to migrate: ${products.length}`);
+    const coupons = await getAllCoupons();
+    console.log(`Total coupons to migrate: ${coupons.length}`);
     const promotionCodes = await getAllPromotionCodes();
     console.log(`Total promotion codes to migrate: ${promotionCodes.length}`);
     const subscriptionSchedules = await getAllScriptionSchedules();
     console.log(
       `Total subscription schedules to migrate: ${subscriptionSchedules.length}`
     );
-    const subscriptions = await getAllSubscriptions();
+    const subscriptions = await getAllSubscriptions(); //can take a while
     console.log(`Total subscriptions to migrate: ${subscriptions.length}`);
-    
+    const invoices = await getAllInvoices(); //can take a while
+    console.log(`Total invoices to migrate: ${invoices.length}`);    
 
     console.log("Inventory process completed.");
   } catch (err) {
