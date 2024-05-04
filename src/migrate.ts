c4e9214981056049d8757c5ceeead6888846e540
bapaero <49796533+helloworld9912@users.noreply.github.com>
2024-05-04 14:16:03 +0200

    Update migrate.ts

--- src/migrate.ts
+++ src/migrate.ts
@@ -1,46 +1,52 @@
-// Importing the migration functions
-import { migrateCoupons } from "./scripts/coupons_codes";
-//import { migrateProducts } from "./scripts/products";
-//import { migratePrices } from "./scripts/prices";
 import { CONFIG } from "./config";
+
+import { migrateCoupons } from "./scripts/coupons_codes";
+import { migrateProducts } from "./scripts/products";
+import { migratePrices } from "./scripts/prices";
 import { migratePromotionCode } from "./scripts/promotion_codes";
 import { migrateScriptionSchedules } from "./scripts/subscription_schedules";
 import { migrateInvoices } from "./scripts/invoices";
 
-// A type for the migration function
-type MigrationFunction = () => Promise<void>;
-
-// Mapping the migration configurations to their corresponding functions
-const migrationTasks: Record<string, MigrationFunction> = {
-  //COUPONS: migrateCoupons, //need : ['products']
-  //PROMOTION_CODES: migratePromotionCode, //need : ['coupons', 'customer']
-  //PRODUCTS: migrateProducts, // need nothing
-  //PRICES: migratePrices, //need : ['products']
-  //SUBSCRIPTIONS: migrateSubscriptions, //need : ['products', 'prices', 'customer']
-  //SUBSCRIPTION_SCHEDULES: migrateScriptionSchedules, //need : ['products', 'prices', 'customer']
-};
-
-
+/**
+ * The migration process need to follow a specific order to avoid any issues.
+ * For example, you can't migrate subscriptions before migrating products and prices.
+ * This is why we need to define the migration tasks in a specific order.
+ * 1. PRODUCTS
+ * 2. PRICES
+ * 3. COUPONS
+ * 4. PROMOTION_CODES
+ * 5. PAYMENT_LINKS
+ * 6. SUBSCRIPTIONS
+ * 7. SUBSCRIPTION_SCHEDULES
+ * 8. INVOICES
+ */
 
 async function startMigration(): Promise<void> {
     console.log("Starting the migration process of your Stripe data...");
-    //await migrateCoupons();
-    //await migrateScriptionSchedules();
-    await migrateInvoices();
-    /*
-    for (const [key, isEnabled] of Object.entries(CONFIG.MIGRATE)) {
-      if (isEnabled) {
-        console.log(`Starting the migration of ${key.toLowerCase()}...`);
-        try {
-          await migrationTasks[key](); // Execute the corresponding migration function
-          console.log(`Migration of ${key.toLowerCase()} completed with success.`);
-        } catch (err) {
-          console.error(`Migration of ${key.toLowerCase()} failed!`);
-          console.error(err);
-        }
-      }
+
+    if(CONFIG.MIGRATE.PRODUCTS) {
+      await migrateProducts();
     }
-    */
+
+    if(CONFIG.MIGRATE.PRICES) {
+      await migratePrices();
+    }
+
+    if(CONFIG.MIGRATE.COUPONS) {
+      await migrateCoupons();
+    }
+
+    if(CONFIG.MIGRATE.PROMOTION_CODES) {
+      await migratePromotionCode();
+    }
+
+    if(CONFIG.MIGRATE.SUBSCRIPTION_SCHEDULES) {
+      await migrateScriptionSchedules();
+    }
+
+    if(CONFIG.MIGRATE.INVOICES) {
+      await migrateInvoices();
+    }
   
     console.log("Migration process completed.");
   }
