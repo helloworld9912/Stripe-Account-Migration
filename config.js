1a15e93e7e532ec623d22f7d8ac834dc27309230
bapaero <49796533+helloworld9912@users.noreply.github.com>
2024-05-05 00:35:22 +0200

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-const CONFIG = { 
-    MIGRATE: { // define the migration settings (what you want to migrate)
-        COUPONS: false,
-        PRODUCTS: false,
-        PRICES: true,
-    }
-}
-
-module.exports = { CONFIG };
